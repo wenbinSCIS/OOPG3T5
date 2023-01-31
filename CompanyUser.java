ff8c398a8225bc265f3891aa02d1eaeb6b1c6573
Bak Wen Yan <bakwenyan@gmail.com>
2023-01-31 17:39:37 +0800

    Added new ArrayList form_list to CompanyUser.java
    Added form_list and method to add form object to form_list

--- CompanyUser.java
+++ CompanyUser.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+
 public class CompanyUser extends User{
+    private ArrayList<Form> form_list;
 
     public CompanyUser(String username,String password)
     {
         super(username, password);
+        this.form_list = new ArrayList<Form>();
+    }
+
+    public ArrayList<Form> getFormList() 
+    {
+        return this.form_list;
+    }
+
+    public void AddFormToFormList(Form form)
+    {
+        this.form_list.add(form);
     }
 }
