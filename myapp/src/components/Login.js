a870b8c04cef44315e5d4d11f52091173bf568d7
wenbinSCIS <wenbin.ong.2020@scis.smu.edu.sg>
2023-02-14 13:13:57 +0800

    updates to login
    changed the background to a mp4

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -11,6 +11,8 @@ import {
 from 'mdb-react-ui-kit';
 import logo from "../assets/img/log.jpg";
 import {Routes, Route, useNavigate} from 'react-router-dom';
+import backgroundVideo from '../assets/video/videofile3.mp4';
+
 
 export default function Login() {
     const navigate = useNavigate();
@@ -58,15 +60,13 @@ export default function Login() {
         </MDBCol>
 
         <MDBCol col='6' className="mb-5">
-          <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
+          
 
-            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-              <h4 className="mb-4">QUANTUM LEAP Portal</h4>
-              <p className="small mb-0">Do everything in here!
-              </p>
-            </div>
+            <video autoPlay loop muted id='video'>
+                <source src={backgroundVideo} type ='video/mp4'/>
+            </video>
 
-          </div>
+         
 
         </MDBCol>
 
