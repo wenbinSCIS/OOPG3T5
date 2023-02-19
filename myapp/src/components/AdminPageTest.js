6e00075be8638b28f1f403fc7509d71f75541e7f
Juay En <juayen.ho.2020@scis.smu.edu.sg>
2023-02-19 20:20:16 +0800

    updated for section

--- myapp/src/components/AdminPageTest.js
+++ myapp/src/components/AdminPageTest.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import GenerateRow from "./RowGeneration";
+import GenerateSection from "./SectionGeneration";
 import "./AdminPage.css";
 
 function MyForm() {
   const [formComponents, setFormComponents] = useState([]);
+  var each_section = {
+    "sectionName":"Name",
+    "sectionText":"Fill in your name",
+    "sectionFont":"12", 
+    "numRows":"1",
+    "rowElements":[[
+        {
+            "elementName":"firstName",
+            "elementHeader":"Full Name",
+            "placeholder":"First Name",
+            "placeholderPosition":"hint", //either hint or under for now
+            "elementType":"Textinput",
+        },
+        {
+          "elementName":"lastName",
+          "elementHeader":"",
+          "placeholder":"Last Name",
+          "placeholderPosition":"hint",
+          "elementType":"Textinput",
+        }
+    ]]
+}
 
   function handleAddComponent() { // we should specific add components for each type
     setFormComponents([
       ...formComponents,
-      <GenerateRow
-        columns={2}
-        inputType={"Textinput"}
-        details={[
-          ["Full Name", "First Name"],
-          ["", "Last Name"],
-        ]}
-      ></GenerateRow>,
+      <GenerateSection section={each_section}></GenerateSection>,
     ]);
   }
 
