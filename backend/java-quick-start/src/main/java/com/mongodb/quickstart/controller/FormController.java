278c6abb9123c914abd93f63500f9826403e5097
decamarkers <ecyhtots@gmail.com>
2023-02-25 16:49:58 +0800

    added getAllFormsByVersion and getFormByName

--- backend/java-quick-start/src/main/java/com/mongodb/quickstart/controller/FormController.java
+++ backend/java-quick-start/src/main/java/com/mongodb/quickstart/controller/FormController.java
@@ -5,12 +5,14 @@ import com.mongodb.quickstart.repository.FormRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +27,7 @@ public class FormController {
   @Autowired
   FormRepository formRepository;
 
-  @GetMapping("/forms")
+  @GetMapping("/getAllForms")
   public ResponseEntity<List<Form>> getAllForms(@RequestParam(required = false) String formName) {
     try {
         List<Form> forms = new ArrayList<Form>();
@@ -44,8 +46,34 @@ public class FormController {
         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
       }
   }
+
+  @GetMapping("/getFormByName/{formName}")
+  public ResponseEntity<Form> getFormByFormName(@PathVariable("formName") String formName) {
+    Optional<Form> formData = formRepository.findByFormName(formName);
+  
+    if (formData.isPresent()) {
+      return new ResponseEntity<>(formData.get(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @GetMapping("/getAllFormsByVersion/{version}")
+  public ResponseEntity<List<Form>> findByVersion(@PathVariable("version") double version) {
+    try {
+      List<Form> forms = formRepository.findByVersion(version);
+
+      if (forms.isEmpty()) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<>(forms, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   @PostMapping("/createForm")
-    public ResponseEntity<Form> createTutorial(@RequestBody Form form) {
+    public ResponseEntity<Form> createForm(@RequestBody Form form) {
     try {
         Form _form = formRepository.save(new Form(form.getFormName(), form.getSections(), form.getVersion()));
         return new ResponseEntity<>(_form, HttpStatus.CREATED);
--- backend/java-quick-start/src/main/java/com/mongodb/quickstart/repository/FormRepository.java
+++ backend/java-quick-start/src/main/java/com/mongodb/quickstart/repository/FormRepository.java
@@ -1,6 +1,7 @@
 package com.mongodb.quickstart.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -9,4 +10,5 @@ import com.mongodb.quickstart.models.Form;
 public interface FormRepository extends MongoRepository<Form, String> {
   List<Form> findByFormNameContaining(String formName);
   List<Form> findByVersion(double version);
+  Optional<Form> findByFormName(String formName);
 }
