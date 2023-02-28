c2064e2b4f671bef28694def8b2368cb10e86eed
wenbinSCIS <wenbin.ong.2020@scis.smu.edu.sg>
2023-02-28 15:22:44 +0800

    Update FormCard.js

--- myapp/src/components/FormCard.js
+++ myapp/src/components/FormCard.js
@@ -43,8 +43,8 @@ const FormCard = ({ formItems }) => {
               <MDBListGroupItem>Status : <span className={`badge rounded-pill ${pillClass}`}>{item.status}</span></MDBListGroupItem>
             </MDBListGroup>
             <MDBCardBody>
-              <MDBCardLink href='#'>Card link</MDBCardLink>
-              <MDBCardLink href='#'>Card link</MDBCardLink>
+              <MDBCardLink href='#'>Get Started</MDBCardLink>
+          
             </MDBCardBody>
           </MDBCard>
         )
