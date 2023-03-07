1af3d1d21a29d2df72146ca6bcd70309d57d18d3
Juay En <juayen.ho.2020@scis.smu.edu.sg>
2023-03-07 23:23:20 +0800

    Revert "row level working"
    This reverts commit 61eea7ac41a3edc8264580100b2d72d09b34dd57.

--- myapp/src/components/Imtesting/SectionGeneration.js
+++ myapp/src/components/Imtesting/SectionGeneration.js
@@ -1,7 +1,55 @@
 import rowElementsReact, { useState } from 'react';
 import GenerateRow from './RowGeneration';
 
-var prop = 
+var prop = {'section':{
+    "sectionName":"Address",
+    "sectionText":"Fill in your address",
+    "sectionFont":"12", 
+    "numRows":"4",
+    "rowElements":[[
+        {
+            "elementName":"streetAddress1",
+            "elementHeader":"Address",
+            "placeholder":"Street Address",
+            "placeholderPosition":"hint", 
+            "elementType":"Textinput",
+        }
+    ],
+    [
+      {
+          "elementName":"streetAddress2",
+          "elementHeader":"",
+          "placeholder":"Street Address Line 2",
+          "placeholderPosition":"hint", 
+          "elementType":"Textinput",
+      }
+  ],
+  [
+    {
+        "elementName":"city",
+        "elementHeader":"",
+        "placeholder":"city",
+        "placeholderPosition":"hint", 
+        "elementType":"Textinput",
+    },
+    {
+      "elementName":"StateProvince",
+      "elementHeader":"",
+      "placeholder":"State / Province",
+      "placeholderPosition":"hint", 
+      "elementType":"Textinput",
+  }
+],[
+    {
+        "elementName":"How",
+        "elementHeader":"How did you hear about us?*",
+        "elementType":"Dropdown",
+        "size":"4",
+        "options":["Selection A","Selection B","Selection C"]
+    }
+],
+  ]
+}}
 
 
 
