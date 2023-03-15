d58674c02e55c6affc9298e9c3d7cb81759d3622
wenbinSCIS <wenbin.ong.2020@scis.smu.edu.sg>
2023-03-15 13:28:27 +0800

    updates to adminCreation

--- myapp/src/components/AdminCreation.js
+++ myapp/src/components/AdminCreation.js
@@ -3,11 +3,13 @@ import Footer from './Footer';
 import Totop from './Totop';
 import AdminSidebar from './Sidebar/AdminSidebar';
 import Header from './Header';
+import './AdminCreation.css'; 
 
 export default function AdminCreation() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -21,9 +23,13 @@ export default function AdminCreation() {
     setRole(event.target.value);
   };
 
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // TODO: send username, password, and role to backend for processing
+    // TODO: send username, password, email, and role to backend for processing
   };
 
   return (
@@ -34,82 +40,100 @@ export default function AdminCreation() {
           <Header />
           <div className="row">
             <div className="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1">
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <h2
-                  className="text-g"
-                  style={{ fontWeight: 'bold', fontSize: 30, color: 'black' }}
-                >
-                  Account Creation for Vendor
-                </h2>
+              <div className="account-creation">
+              <h2 className="text-g" style={{ fontWeight: 'bold', fontSize: 30, color: 'black' }}>Account Creation</h2>
+                <form onSubmit={handleSubmit} className="account-creation-form">
+                  <div className="form-group input-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text">
+                        <i className="fas fa-user"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      placeholder="Username"
+                      value={username}
+                      onChange={handleUsernameChange}
+                    />
+                  </div>
+                  <div className="form-group input-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text">
+                        <i className="fas fa-envelope"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Email Address"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </div>
+                  <div className="form-group input-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text">
+                        <i className="fas fa-lock"></i>
+                      </span>
+                    </div>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="role">Role:</label>
+                    <div>
+                      <label className="radio-inline" style={{ paddingRight: 10 }}>
+                        <input
+                          type="radio"
+                          name="role"
+                          value="Vendor"
+                          checked={role === 'Vendor'}
+                          onChange={handleRoleChange}
+                          />
+                          Vendor
+                        </label>
+                        <label className="radio-inline" style={{ paddingRight: 10 }}>
+                          <input
+                            type="radio"
+                            name="role"
+                            value="Admin"
+                            checked={role === 'Admin'}
+                            onChange={handleRoleChange}
+                          />
+                          Admin
+                        </label>
+                        <label className="radio-inline" style={{ paddingRight: 10 }}>
+                          <input
+                            type="radio"
+                            name="role"
+                            value="Approver"
+                            checked={role === 'Approver'}
+                            onChange={handleRoleChange}
+                          />
+                          Approver
+                        </label>
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Create User
+                    </button>
+                  </form>
+                </div>
               </div>
-              <form onSubmit={handleSubmit} style={{ marginTop: 50 }}>
-                <div className="form-group">
-                  <label htmlFor="username">Username:</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    value={username}
-                    onChange={handleUsernameChange}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password:</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                  />
-                </div>
-                <div className="form-group">
-  <label htmlFor="role">Role:</label>
-  <div>
-    <label className="radio-inline" style={{ paddingRight: 10 }}>
-      <input
-        
-        type="radio"
-        name="role"
-        value="Vendor"
-        checked={role === 'Vendor'}
-        onChange={handleRoleChange}
-      />
-      Vendor
-    </label>
-    <label className="radio-inline"  style={{ paddingRight: 10 }}>
-      <input
-        type="radio"
-        name="role"
-        value="Admin"
-        checked={role === 'Admin'}
-        onChange={handleRoleChange}
-       
-      />
-      Admin
-    </label>
-    <label className="radio-inline" style={{ paddingRight: 10 }}>
-      <input
-        type="radio"
-        name="role"
-        value="Approver"
-        checked={role === 'Approver'}
-        onChange={handleRoleChange}
-      />
-      Approver
-    </label>
-  </div>
-</div>
-                <button type="submit" className="btn btn-primary">
-                  Create User
-                </button>
-              </form>
             </div>
           </div>
-        </div>
-      </section>
-      <Footer />
-      <Totop />
-    </>
-  );
-}
+        </section>
+        <Footer />
+        <Totop />
+      </>
+        );
+      }
