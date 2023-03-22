f6a824b7a6ce50fe5257ca4dd4929d511752f642
Bak Wen Yan <bakwenyan@gmail.com>
2023-03-22 15:01:14 +0800

    Finish creating Project.java

--- backend/java-quick-start/src/main/java/com/mongodb/quickstart/models/Project.java
+++ backend/java-quick-start/src/main/java/com/mongodb/quickstart/models/Project.java
@@ -3,12 +3,23 @@ package com.mongodb.quickstart.models;
 import java.util.ArrayList;
 
 public class Project {
+    private String projectName;
     private ArrayList<AssignedForm> assignedForm;
     private String status;
 
-    public Project(ArrayList<AssignedForm> assignedForm,String status)
+    public Project(String projectName,ArrayList<AssignedForm> assignedForm,String status)
     {
-        
+        this.projectName = projectName;
+        this.assignedForm = assignedForm;
+        this.status = status;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
     }
 
     public ArrayList<AssignedForm> getAssignedForm() {
@@ -17,5 +28,13 @@ public class Project {
 
     public void setAssignedForm(ArrayList<AssignedForm> assignedForm) {
         this.assignedForm = assignedForm;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
