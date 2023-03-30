7eb8be91d56ca4c2db9b15fb2458017501d064bd
wenbinSCIS <wenbin.ong.2020@scis.smu.edu.sg>
2023-03-30 18:31:33 +0800

    nav

--- myapp/src/components/Sidebar/AdminSidebar.js
+++ myapp/src/components/Sidebar/AdminSidebar.js
@@ -97,7 +97,7 @@ const linksArray = [
     {
         label: "Create Forms",
         icon: <MdBuild />,
-        to: "./AdminPageComponents/FormSelector",
+        to: "/testadmin",
       
     },
     {
