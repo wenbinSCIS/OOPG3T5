c4e3459131447240c95ce97572b4307740a5cf4a
Liam Wells Ayathan <ayathan21@gmail.com>
2023-04-02 06:32:33 +0800

    small change to preview tab

--- myapp/src/components/SectionEditor/PreviewTab.js
+++ myapp/src/components/SectionEditor/PreviewTab.js
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
-import GenerateSection from '../SectionGeneration';
+import React, { useState } from "react";
+import GenerateSection from "../SectionGeneration";
 
-export default function PreviewTab({sectionState, handleDelete}) {
-  console.log(sectionState)
+export default function PreviewTab({ sectionState, handleDelete }) {
+  console.log("in the preview tab the section state is", sectionState);
+
   var [allData, setallData] = useState({}); //All data to save for user
   return (
     <div>
       <h5>Preview Tab</h5>
-      {typeof sectionState.rowElements === "undefined" ||
-        (sectionState.rowElements.length === 0 && (
+      {typeof sectionState.rowElements === "undefined" && (
+        <p>The Section is currently Empty</p>
+      )}
+      {typeof sectionState.rowElements !== "undefined" &&
+        sectionState.rowElements.length === 0 && (
           <p>The Section is currently Empty</p>
-        ))}
+        )}
       {typeof sectionState.rowElements !== "undefined" &&
         sectionState.rowElements.length !== 0 && (
           <div>
