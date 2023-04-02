5fec069323b2de72678129701c3f242048c5e895
wenbinSCIS <wenbin.ong.2020@scis.smu.edu.sg>
2023-04-02 14:57:44 +0800

    Update MyRoute.js

--- myapp/src/router/MyRoute.js
+++ myapp/src/router/MyRoute.js
@@ -1,10 +1,6 @@
 import { Routes , Route } from "react-router-dom";
-
-
 import Home from "../components/Home";
 import Login from "../components/Login";
-
-
 import ApprovalList from "../components/ApprovalList";
 import AdminApprovalList from "../components/AdminApprovalList";
 import AdminCreation from "../components/AdminCreation";
@@ -27,7 +23,7 @@ export default function MyRoute() {
         <Route exact path="/ApprovalList" element={<ApprovalList/>} />
         <Route exact path="/AssignForm" element={<AssignForm/>} />
         <Route exact path="/AdminCreation" element={<AdminCreation/>} />
-        <Route exact path="/testpage" element={<TestPage/>} /> 
+       
         <Route exact path="/vendor" element={<VendorAssessmentForm/>} />
         <Route exact path="/vendorApprover" element={<VendorAssessmentFormApprover/>} />
         <Route exact path="/testadmin" element={<MyForm/>}/> 
